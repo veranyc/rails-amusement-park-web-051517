eae5ed1622c1f6352c5778106a8ee78e345770cc
veranyc <veranyc@gmail.com>
2017-06-15 11:30:42 -0400

    Updated User Admin with default false

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,4 +1,13 @@
 class Ride < ActiveRecord::Base
   belongs_to :user
   belongs_to :attraction
+
+  def take_ride
+    #accounts for the user not having enough tickets
+    #accounts for the user not being tall enough
+    #accounts for the user not being tall enough and not having enough tickets
+    #updates ticket number
+    #updates the user's nausea
+    #updates the user's happiness 
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :rides
   has_many :attractions, through: :rides
+
+  def mood
+    #returns 'sad' when the user is more nauseous than happy
+    #returns 'happy' when the user is more happy than nauseous 
+  end
 end
